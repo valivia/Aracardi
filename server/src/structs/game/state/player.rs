a20bc2526d797f6e90c00bc2d7ea875dbe5189b1
valivia <owl@MacBookAir.domain_not_set.invalid>
2026-04-20 14:00:58 +0200

    🛠️ Statics for name length limits

--- server/src/structs/game/state/player.rs
+++ server/src/structs/game/state/player.rs
@@ -1,6 +1,15 @@
 use serde::{Deserialize, Serialize};
 use serde_with::skip_serializing_none;
 
+const MIN_PLAYER_NAME_LENGTH: usize = 3;
+const MAX_PLAYER_NAME_LENGTH: usize = 20;
+
+// TODO: possibly check the actual avatar list?
+const MIN_PLAYER_AVATAR_LENGTH: usize = 3;
+const MAX_PLAYER_AVATAR_LENGTH: usize = 20;
+
+const PLAYER_ID_LENGTH: usize = 16;
+
 #[skip_serializing_none]
 #[derive(Serialize, Deserialize, Clone, Debug)]
 #[serde(rename_all = "camelCase")]
@@ -17,8 +26,8 @@ pub struct Player {
 
 impl Player {
     pub fn is_valid(&self) -> bool {
-        return (3..20).contains(&self.name.len())
-            && (3..20).contains(&self.avatar.len())
-            && self.id.len() == 16;
+        return (MIN_PLAYER_NAME_LENGTH..MAX_PLAYER_NAME_LENGTH).contains(&self.name.len())
+            && (MIN_PLAYER_AVATAR_LENGTH..MAX_PLAYER_AVATAR_LENGTH).contains(&self.avatar.len())
+            && self.id.len() == PLAYER_ID_LENGTH;
     }
 }
